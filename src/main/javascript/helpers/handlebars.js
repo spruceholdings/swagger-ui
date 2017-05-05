a5b9e59dd042f8f6f76ac82fe0c63582c071e18a
trinityxmontoya <trinityXmontoya@gmail.com>
2017-05-05 17:17:48 -0400

    Different formatting for intro section

--- src/main/javascript/helpers/handlebars.js
+++ src/main/javascript/helpers/handlebars.js
@@ -1,7 +1,13 @@
 'use strict';
 
 Handlebars.registerHelper('sanitize', function(html) {
-    // Strip the script tags from the html, and return it as a Handlebars.SafeString
-    html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-    return new Handlebars.SafeString(html);
+  // Strip the script tags from the html, and return it as a Handlebars.SafeString
+  html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+  return new Handlebars.SafeString(html);
+});
+
+Handlebars.registerHelper('ifneq', function(a, b, options) {
+  if (a != b) {
+    return options.fn(this);
+  }
 });
